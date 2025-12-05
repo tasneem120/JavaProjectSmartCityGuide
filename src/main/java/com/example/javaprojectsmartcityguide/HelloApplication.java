a2812eb49c47fc094f56be340428999e024ce14e
tasneem <ktasneem074@gmail.com>
2025-12-05 15:39:37 +0200

    JDBC

--- src/main/java/com/example/javaprojectsmartcityguide/HelloApplication.java
+++ src/main/java/com/example/javaprojectsmartcityguide/HelloApplication.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,6 +21,17 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        String url = "jdbc:mysql://localhost:3306/smartcityguide";
+        String username = "tasnim";
+        String password = "123";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection successful!");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.javaprojectsmartcityguide {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.javaprojectsmartcityguide to javafx.fxml;
     exports com.example.javaprojectsmartcityguide;
